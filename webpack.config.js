d0736cf06fa2b3dfdfaf1b714b6a8970388f1c3e
Kim Joon Su <padomay1352@gmail.com>
2023-03-01 20:42:37 +0900

    프로젝트 설정 완료

--- webpack.config.js
+++ webpack.config.js
@@ -3,27 +3,55 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
+const isProduction = process.argv[process.argv.indexOf('--mode') + 1] === 'production';
+
 module.exports = {
   mode: "development", //개발용이나 프로덕션용이냐
   entry: {
-    app: "./src/app.js",
+    app: "./src/app.ts",
   },
   output: {
-    //여러개의 모듈을 하나로 만들어서 저장시킬 위치를 설정
+    //여러개의 모듈을 하나로 만들어서 저장시킬, 위치를 설정
     publicPath: "/",
     filename: "[name].js",
-    path: path.resolve("./dist"),
+    path: path.resolve(__dirname, "./dist"),
     clean: true,
   },
+  resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src/"),
+      },
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devtool: 'inline-source-map',
   devServer: {
     port: 9000,
+    historyApiFallback: true
   },
   module: {
-    // Bundle styles into main.css
     rules: [
       {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [isProduction ? MiniCssExtractPlugin.loader : "style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
     ],
   },
